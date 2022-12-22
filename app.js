d68a91e93e32e1e94e5560ee3a20bbb3564d122f
shiroyahardik <shiroyahardik86@gmail.com>
2022-12-22 15:28:36 +0530

    updatev3

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-import 'dotenv/config'
+//import 'dotenv/config'
+require('dotenv').config();
 import express from 'express'
 import nodeCleanup from 'node-cleanup'
 import routes from './routes.js'
